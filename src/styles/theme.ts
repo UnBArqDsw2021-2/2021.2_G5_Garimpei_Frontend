c6790235af4707a3881deba6f7eb85e6ff1369cc
Paulo Victor <paulovmel@gmail.com>
2022-03-07 22:35:37 -0300

    style: updates theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      500: "#DB2F44",
+      600: "#9d1b2b",
+      700: "#71131f",
+    },
+  },
+  shadows: {
+    outline: "0 0 0 3px #DB2F44",
+  },
+});
