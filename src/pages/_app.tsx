b4239c55d15aacc81d3dc2dd20685e7fbd768c9c
Denys Rógeres <dennysrogeres@gmail.com>
2022-03-15 23:40:35 -0300

    feat:remove import headerComponent in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../styles/theme";
-import { HeaderComponent } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
