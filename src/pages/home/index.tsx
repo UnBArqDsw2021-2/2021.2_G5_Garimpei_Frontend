c132e01e32e700e1a721d95130bd2b4c31f9ca15
Juliana Valle <juju.pvg@gmail.com>
2022-03-17 00:27:53 -0300

    chore: UnBArqDsw2021-2/2021.2_G5_Garimpei#114 create home page
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Paulo Victor <paulovmel@gmail.com>

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,62 @@
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { ButtonComponent } from "../../components/Button";
+import { CategoryCard } from "../../components/CategoryCard";
+import { Footer } from "../../components/Footer";
+import { HeaderComponent } from "../../components/Header";
+
+export default function Home() {
+  return (
+    <Box h="100vh" w="full" maxW="1440px" margin="0 auto">
+      <HeaderComponent />
+
+      <Box bg="gray" w="100%" h="250px" mt="5.625rem" borderRadius="8px"></Box>
+
+      <Text fontSize="64px" mb="1.25rem">
+        categorias
+      </Text>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap="1.5rem">
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Roupas
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Camisetas
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Moletons
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Óculos
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Acessórios
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Bolsas
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Calçados
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Vestidos
+        </CategoryCard>
+
+        <CategoryCard imageUrl="https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80">
+          Calças
+        </CategoryCard>
+      </SimpleGrid>
+
+      <Flex mt={{ base: "1.5rem", lg: "5rem" }} justifyContent="flex-end">
+        <ButtonComponent>Anunciar agora</ButtonComponent>
+      </Flex>
+
+      <Footer />
+    </Box>
+  );
+}
