a71198e4d3e01f0f535b8e5e9b81ef2ddd337dde
Paulo Victor <paulovmel@gmail.com>
2022-03-18 01:36:37 -0300

    feat: fix footer position on screen
    Co-authored-by: Daniel Barcelos <danielbarcelos.7@hotmail.com>

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,28 +1,24 @@
-import {
-    Container,
-    Stack,
-    Text,
-    Flex,
-    HStack
-  } from '@chakra-ui/react';
+import { Container, Stack, Text, Flex, HStack } from "@chakra-ui/react";
 
-export function Footer(){
-    return(
-        
-        <Flex bg="dark.500">
-            <Container
-                as={Stack}
-                maxW={'2xl'}
-                py={6}
-                justify={{ base: 'center', md: 'space-between' }}
-                align={{ base: 'center', md: 'center' }}>
-
-                <HStack justifyContent={'center'}>
-                    <Text color="white" fontSize={'xs'}>© Powered By </Text>
-                    <Text color="brand.500" fontSize={'xs'} fontWeight='bold'>Garimpei Team</Text>
-                </HStack>
-
-            </Container>
-        </Flex>
-    );
+export function Footer() {
+  return (
+    <Flex bg="dark.500" w="full" position="absolute" bottom="0" left="0">
+      <Container
+        as={Stack}
+        maxW={"2xl"}
+        py={6}
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <HStack justifyContent={"center"}>
+          <Text color="white" fontSize={"xs"}>
+            © Powered By{" "}
+          </Text>
+          <Text color="brand.500" fontSize={"xs"} fontWeight="bold">
+            Garimpei Team
+          </Text>
+        </HStack>
+      </Container>
+    </Flex>
+  );
 }
