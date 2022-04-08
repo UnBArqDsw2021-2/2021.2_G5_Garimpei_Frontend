330fdceb68d6e1a77f0ee4e651618c77a6a59fce
Paulo Victor <paulovmel@gmail.com>
2022-04-08 00:31:05 -0300

    feat: updates Community Users page
    Co-authored-by: Thiago Mesquita <thiago099carvalho@gmail.com>

--- src/pages/community/users.tsx
+++ src/pages/community/users.tsx
@@ -1,4 +1,5 @@
-import { Divider, Heading } from "@chakra-ui/react";
+import { Avatar, Divider, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import { CommunityUser } from "../../components/CommunityUser";
 import { DashboardLayout } from "../../components/DashboardLayout";
 
 export default function ManageComunity() {
@@ -7,6 +8,55 @@ export default function ManageComunity() {
       <Heading>Gerenciar comunidade</Heading>
 
       <Divider w="full" opacity={1} />
+
+      <Grid
+        gridTemplateColumns="repeat( auto-fit, minmax(150px, 1fr) )"
+        rowGap="1.5rem"
+      >
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+        <CommunityUser
+          userID="1"
+          username="Segun Adebayo"
+          avatarURL="https://bit.ly/sage-adebayo"
+        />
+      </Grid>
     </DashboardLayout>
   );
 }
