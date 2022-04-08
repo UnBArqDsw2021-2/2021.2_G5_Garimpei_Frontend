99add902691c5326647ddedbf3a4fff696a21671
Paulo Victor <paulovmel@gmail.com>
2022-04-08 00:00:11 -0300

    feat: creates Community Users page
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>

---
+++ src/pages/community/users.tsx
@@ -0,0 +1,12 @@
+import { Divider, Heading } from "@chakra-ui/react";
+import { DashboardLayout } from "../../components/DashboardLayout";
+
+export default function ManageComunity() {
+  return (
+    <DashboardLayout>
+      <Heading>Gerenciar comunidade</Heading>
+
+      <Divider w="full" opacity={1} />
+    </DashboardLayout>
+  );
+}
