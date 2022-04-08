f12e1f0121a0cc7aef1204d1be07233ef3a5f0e1
Paulo Victor <paulovmel@gmail.com>
2022-04-08 00:00:35 -0300

    feat: creates Community Users page
    Co-authored-by: Victor Gonçalves <victor.eng.unb@gmail.com>

---
+++ src/pages/dashboard/purchases.tsx
@@ -0,0 +1,10 @@
+import { Heading } from "@chakra-ui/react";
+import { DashboardLayout } from "../../components/DashboardLayout";
+
+export default function Purchases() {
+  return (
+    <DashboardLayout>
+      <Heading>Minhas compras</Heading>
+    </DashboardLayout>
+  );
+}
