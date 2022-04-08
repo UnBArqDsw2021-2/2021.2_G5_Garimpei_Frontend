de0fd1b4561cd552f04374f696abe08106ab2091
Paulo Victor <paulovmel@gmail.com>
2022-04-07 23:30:58 -0300

    refactor: set_password page

--- src/pages/auth/set_password.tsx
+++ src/pages/auth/set_password.tsx
@@ -12,7 +12,6 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
-import { MdOutlineEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 
 export default function SetPassword() {
