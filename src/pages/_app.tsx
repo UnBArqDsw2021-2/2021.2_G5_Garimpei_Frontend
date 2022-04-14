e3cb6748141fd7b8191940c6c54b479b37402593
Paulo Victor <paulovmel@gmail.com>
2022-04-14 11:16:36 -0300

    feat: adds loading screen between pages
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,45 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "../styles/theme";
+import { Loading } from "../components/Loading";
+import { useCallback, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  console.log(router.events);
+
+  const handleRouteChange = useCallback((event) => {
+    console.log(event);
+    if (event === "start") {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => handleRouteChange("start"));
+    router.events.on("routeChangeComplete", () =>
+      handleRouteChange("complete")
+    );
+    router.events.on("routeChangeError", () => handleRouteChange("error"));
+
+    return () => {
+      router.events.off("routeChangeStart", () => handleRouteChange("teste"));
+      router.events.off("routeChangeComplete", () =>
+        handleRouteChange("complete")
+      );
+      router.events.off("routeChangeError", () =>
+        handleRouteChange("complete")
+      );
+    };
+  }, [router.events, handleRouteChange]);
+
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      {loading ? <Loading /> : <Component {...pageProps} />}
     </ChakraProvider>
   );
 }
