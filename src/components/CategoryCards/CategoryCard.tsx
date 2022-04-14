c2e37bb0d83fee7cf74430df546884f60e706e11
Paulo Victor <paulovmel@gmail.com>
2022-04-14 00:06:24 -0300

    feat: adds new images into CategoryCard
    Co-authored-by: Denys Rogeres <dennysrogeres@gmail.com>

--- src/components/CategoryCards/CategoryCard.tsx
+++ src/components/CategoryCards/CategoryCard.tsx
@@ -13,9 +13,7 @@ export function CategoryCard({ imageUrl, children }: CategoryCardProps) {
       justifyContent="center"
       background={`url(${imageUrl}) center/cover no-repeat`}
       h="15rem"
-      // w="400px"
       borderRadius="10px"
-      boxShadow="inset 0 0 0 2000px rgba(0, 0, 0, 0.6)"
     >
       <Text fontFamily="bold" fontSize="48px" color="light.200">
         {children}
