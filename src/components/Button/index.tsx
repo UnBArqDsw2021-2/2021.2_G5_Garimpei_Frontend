5764155ff2aa11fee78baa6065af7b38e1bc33e1
Paulo Victor <paulovmel@gmail.com>
2022-04-14 11:14:12 -0300

    refactor: button component
    Co-authored-by: Mateus Gomes <mateusgomes.nascimento21@gmail.com>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,27 +4,26 @@ type ButtonProps = {
   children: React.ReactNode;
   isOutline?: boolean;
   color?: string;
-}
+};
 
 export function ButtonComponent(props: ButtonProps) {
-
   return (
     <Button
       colorScheme={props.color}
       _hover={{
         backgroundColor: `${props.color}.600`,
         color: props.isOutline && `${props.color}.700`,
-        filter: props.isOutline && 'brightness(200%)'
+        filter: props.isOutline && "brightness(200%)",
       }}
       _focus={{ boxShadow: `0 0 0 3px ${props.color}.600` }}
-      variant= {props.isOutline ? 'outline' : 'solid'}
-      >
+      variant={props.isOutline ? "outline" : "solid"}
+    >
       {props.children}
     </Button>
   );
 }
 
 ButtonComponent.defaultProps = {
-  color: 'brand',
-  isOutline: false
-}
+  color: "brand",
+  isOutline: false,
+};
