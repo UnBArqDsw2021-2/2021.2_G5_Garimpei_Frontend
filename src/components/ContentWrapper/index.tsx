6b7453534f1318d5ac71c55dae1022f7fc417249
Paulo Victor <paulovmel@gmail.com>
2022-04-16 15:43:59 -0300

    feat: updats ContentWrapper
    Co-authored-by: Victor Gonçalves <victor.eng.unb@gmail.com>

--- src/components/ContentWrapper/index.tsx
+++ src/components/ContentWrapper/index.tsx
@@ -1,5 +1,7 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
 
 interface ContentWrapperProps {
   children: ReactNode;
@@ -7,8 +9,18 @@ interface ContentWrapperProps {
 
 export function ContentWrapper({ children }: ContentWrapperProps) {
   return (
-    <Box w="full" maxW="1120px" mx="auto" py="2rem">
-      {children}
-    </Box>
+    <>
+      <Header />
+      <Flex
+        flexDir="column"
+        justifyContent="space-between"
+        minH="calc(100vh - 5.625rem)"
+      >
+        <Box w="full" maxW="1440px" mx="auto" py="2rem" pb="full">
+          {children}
+        </Box>
+        <Footer />
+      </Flex>
+    </>
   );
 }
