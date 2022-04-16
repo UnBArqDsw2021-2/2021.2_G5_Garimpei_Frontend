07db8ccd9b2b0c61e32e810f3fd59314dd65ef85
Paulo Victor <paulovmel@gmail.com>
2022-04-16 15:49:11 -0300

    feat: updates home page
    Co-authored-by: Denys Rogeres <dennysrogeres@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
-import { Box, Button, Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { ButtonComponent } from "../components/Button";
 import { Carousel } from "../components/Carousel";
 import { CategoryCards } from "../components/CategoryCards";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
+import { ContentWrapper } from "../components/ContentWrapper";
 
 export default function Home() {
   const router = useRouter();
@@ -13,35 +11,30 @@ export default function Home() {
     router.push(path);
   }
   return (
-    <>
-      <Header />
-
-      <Box w="full" maxW="1440px" margin="0 auto">
-        <Box w="100%" mt="5.625rem" mb="2.5rem" borderRadius="8px" px="2rem">
-          <Carousel />
-        </Box>
-
-        <Box px="1rem">
-          <Text fontSize="64px" mb="1.25rem">
-            categorias
-          </Text>
-
-          <CategoryCards />
-
-          <Flex mt={{ base: "1.5rem", lg: "5rem" }} justifyContent="flex-end">
-            <Button
-              bg="brand.500"
-              color="white"
-              _hover={{ bg: "brand.600" }}
-              onClick={() => handleRedirect("products/new")}
-            >
-              Anunciar agora
-            </Button>
-          </Flex>
-        </Box>
+    <ContentWrapper>
+      <Box w="100%" mt="5.625rem" mb="2.5rem" borderRadius="8px" px="2rem">
+        <Carousel />
       </Box>
 
-      <Footer />
-    </>
+      <Box px="1rem">
+        <Text fontSize="64px" mb="1.25rem">
+          categorias
+        </Text>
+
+        <CategoryCards />
+
+        <Flex mt={{ base: "1.5rem", lg: "5rem" }} justifyContent="flex-end">
+          <Button
+            bg="brand.500"
+            color="white"
+            _hover={{ bg: "brand.600" }}
+            onClick={() => handleRedirect("products/new")}
+            mb="3rem"
+          >
+            Anunciar agora
+          </Button>
+        </Flex>
+      </Box>
+    </ContentWrapper>
   );
 }
