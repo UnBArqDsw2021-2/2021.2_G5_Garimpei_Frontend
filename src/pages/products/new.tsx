db49b888994b3449eae47579b869c58557c0266b
Paulo Victor <paulovmel@gmail.com>
2022-04-16 15:46:48 -0300

    feat: add product page
    Co-authored-by: Juliana Valle <juju.pvg@gmail.com>

--- src/pages/products/new.tsx
+++ src/pages/products/new.tsx
@@ -5,49 +5,40 @@ import {
   Heading,
   Icon,
   Input,
-  Text,
   Textarea,
   VStack,
 } from "@chakra-ui/react";
 import { ContentWrapper } from "../../components/ContentWrapper";
-import { Footer } from "../../components/Footer";
-import { Header } from "../../components/Header";
 import { AiOutlinePlus } from "react-icons/ai";
 
 export default function NewProduct() {
   return (
-    <>
-      <Header />
-
-      <ContentWrapper>
-        <Heading>novo anúncio</Heading>
-        <Flex mt="4rem" justifyContent="center" gap="2rem">
-          <Box
-            w="12rem"
-            h="16rem"
-            border="1px dashed #adadad"
-            borderRadius="lg"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            _hover={{ cursor: "pointer" }}
-          >
-            <Icon as={AiOutlinePlus} color="#adadad" fontSize="6rem" />
-          </Box>
-          <Flex flexDirection="column" gap="1rem" minW="18rem">
-            <VStack>
-              <Input shadow="md" placeholder="Título do anúncio" />
-              <Input shadow="md" placeholder="Valor" />
-              <Textarea shadow="md" placeholder="Descrição" />
-            </VStack>
-            <Button bg="brand.500" color="white" _hover={{ bg: "brand.600" }}>
-              Anunciar
-            </Button>
-          </Flex>
+    <ContentWrapper>
+      <Heading>novo anúncio</Heading>
+      <Flex mt="4rem" justifyContent="center" gap="2rem">
+        <Box
+          w="12rem"
+          h="16rem"
+          border="1px dashed #adadad"
+          borderRadius="lg"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          _hover={{ cursor: "pointer" }}
+        >
+          <Icon as={AiOutlinePlus} color="#adadad" fontSize="6rem" />
+        </Box>
+        <Flex flexDirection="column" gap="1rem" minW="18rem">
+          <VStack>
+            <Input shadow="md" placeholder="Título do anúncio" />
+            <Input shadow="md" placeholder="Valor" />
+            <Textarea shadow="md" placeholder="Descrição" />
+          </VStack>
+          <Button bg="brand.500" color="white" _hover={{ bg: "brand.600" }}>
+            Anunciar
+          </Button>
         </Flex>
-      </ContentWrapper>
-
-      <Footer />
-    </>
+      </Flex>
+    </ContentWrapper>
   );
 }
