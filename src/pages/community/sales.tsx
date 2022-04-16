210f5e653ac65a29d68f7f7b36e6d38555f22b26
Paulo Victor <paulovmel@gmail.com>
2022-04-16 15:48:59 -0300

    feat: creates 'Minhas Vendas' page
    Co-authored-by: Denys Rogeres <dennysrogeres@gmail.com>

--- src/pages/community/sales.tsx
+++ src/pages/community/sales.tsx
@@ -1,12 +1,67 @@
-import { Divider, Heading } from "@chakra-ui/react";
+import { Divider, Grid, Heading } from "@chakra-ui/react";
 import { DashboardLayout } from "../../components/DashboardLayout";
+import { ProductWithRatingCard } from "../../components/ProductCard/ProductWithRatingCard";
 
 export default function CommunitySales() {
   return (
     <DashboardLayout>
       <Heading>Vendas da comunidade</Heading>
 
-      <Divider w="full" opacity={1} />
+      <Divider w="full" opacity={1} mt="1rem" mb="2rem" />
+
+      <Grid gap="2rem" mb="3rem">
+        <ProductWithRatingCard
+          imageURL="http://simpleicon.com/wp-content/uploads/picture.png"
+          productName="Nome do produto"
+          productDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+          viverra tortor, ut venenatis turpis. Ut dapibus iaculis diam id
+          vestibulum. Morbi efficitur id leo bibendum laoreet. Curabitur
+          vehicula lacus id est luctus dignissim."
+          purchasedData="03/02/2022"
+          rating={4.3}
+          hasRating
+          isCommunitySale
+        />
+
+        <ProductWithRatingCard
+          imageURL="http://simpleicon.com/wp-content/uploads/picture.png"
+          productName="Nome do produto"
+          productDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+          viverra tortor, ut venenatis turpis. Ut dapibus iaculis diam id
+          vestibulum. Morbi efficitur id leo bibendum laoreet. Curabitur
+          vehicula lacus id est luctus dignissim."
+          purchasedData="03/02/2022"
+          rating={4.3}
+          hasRating
+          isCommunitySale
+        />
+
+        <ProductWithRatingCard
+          imageURL="http://simpleicon.com/wp-content/uploads/picture.png"
+          productName="Nome do produto"
+          productDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+          viverra tortor, ut venenatis turpis. Ut dapibus iaculis diam id
+          vestibulum. Morbi efficitur id leo bibendum laoreet. Curabitur
+          vehicula lacus id est luctus dignissim."
+          purchasedData="03/02/2022"
+          rating={2}
+          hasRating
+          isCommunitySale
+        />
+
+        <ProductWithRatingCard
+          imageURL="http://simpleicon.com/wp-content/uploads/picture.png"
+          productName="Nome do produto"
+          productDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et
+          viverra tortor, ut venenatis turpis. Ut dapibus iaculis diam id
+          vestibulum. Morbi efficitur id leo bibendum laoreet. Curabitur
+          vehicula lacus id est luctus dignissim."
+          purchasedData="03/02/2022"
+          rating={5}
+          hasRating
+          isCommunitySale
+        />
+      </Grid>
     </DashboardLayout>
   );
 }
